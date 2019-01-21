29f2cce7c4467dd6cd15f4f651d5e0d91eae31cd
samjoshuva <sam613263@gmail.com>
2019-01-21 21:20:08 +0530

    hg

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var authHelper = require('./microsoft');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+   let parms = {
+      title: 'Home',
+      active: {
+         home: true
+      }
+   };
+
+   parms.signInUrl = authHelper.getAuthUrl();
+   parms.debug = parms.signInUrl;
+   res.render('index', parms);
+});
+
+module.exports = router;
